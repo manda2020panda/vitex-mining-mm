e2c0ba062adc2b01c234856a45e35e2b53fca08f
manda2020panda <manda2020panda@gmail.com>
2022-08-17 02:11:28 -0500

    configuring

--- mining-mm/src/main/java/org/vite/dex/mm/controller/MiningController.java
+++ mining-mm/src/main/java/org/vite/dex/mm/controller/MiningController.java
@@ -1,6 +1,7 @@
 package org.vite.dex.mm.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,34 +10,48 @@ import org.vite.dex.mm.http.ResultBean;
 import org.vite.dex.mm.http.ResultCode;
 import org.vite.dex.mm.reward.RewardEngine;
 
-import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/v1")
 @Slf4j
 public class MiningController {
+    /**
+     *
+     */
+    private static final String DAILY_RUN_E = "/daily/run, e:{}";
     @Resource
-    private RewardEngine engine;
-
+    private final RewardEngine
+    /**
+     * @return 
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value = "/daily/run", method = RequestMethod.GET)
     public ResultBean invokeDailyRun() {
         try {
-            engine.runDaily();
-            return new ResultBean<>(ResultCode.SUCCESS, "ok", null);
-        } catch (Exception e) {
-            log.error("/daily/run, e:{}", e);
-            return new ResultBean<>(ResultCode.ERROR, e.getMessage(), null);
+            extracted();
+            return new ResultBean <>(ResultCode.SUCCESS, "ok", null);
+        } catch (final Exception e) {
+            log.error(DAILY_RUN_E, e);
+            return new ResultBean <>(ResultCode.ERROR, e.getMessage(), null);
         }
     }
 
+    private void extracted() throws Exception {
+        engine.runDaily();
+    }
+
+    /**
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value = "/halfHour/run", method = RequestMethod.GET)
     public ResultBean invokeHalfHourRun() {
         try {
             engine.runHalfHour();
             return new ResultBean<>(ResultCode.SUCCESS, "ok", null);
-        } catch (Exception e) {
+        } catch (final Exception e) {
             log.error("/halfHour/run, e:{}", e);
             return new ResultBean<>(ResultCode.ERROR, e.getMessage(), null);
         }
